69714f86c298684485219c0ba9c8d414c28fa199
Miguel Medina Ballesteros <mmedina@financialforce.com>
2020-05-02 22:59:37 +0200

    ADD SceneLoader by pressing space

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	static int currentScene;
+
+	public KeyCode nextSceneKey = KeyCode.Space;
+
+	void Start()
+	{
+		currentScene = SceneManager.GetActiveScene().buildIndex;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(nextSceneKey))
+		{
+			var nextScene = (currentScene + 1) % SceneManager.sceneCountInBuildSettings;
+			SceneManager.LoadScene(nextScene);
+		}
+	}
+}
